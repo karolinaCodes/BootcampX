2c942a5cb6f52e782160cdcefeacb226b471e57f
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-11-22 12:52:52 -0500

    clean up dead code

--- 3_queries_group-by/4_total_assingment_submissions.sql
+++ 3_queries_group-by/4_total_assingment_submissions.sql
@@ -4,11 +4,3 @@ JOIN students ON students.id = assignment_submissions.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
 GROUP BY cohorts.name
 ORDER BY total_submissions DESC;
-
-
-SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration
-FROM students
-JOIN assignment_submissions ON student_id =students.id
-WHERE students.end_date IS NULL
-GROUP BY student
-ORDER BY average_assignment_duration DESC;
