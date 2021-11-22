3221e51227fc437475da2ddb47f16c392c115c99
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-11-22 12:14:52 -0500

    fix query 2 and 3 bugs

--- 2_queries_joins/2_students_total_assignment_duration.sql
+++ 2_queries_joins/2_students_total_assignment_duration.sql
@@ -1,4 +1,7 @@
 SELECT SUM(assignment_submissions.duration) as total_duration
-FROM students 
-JOIN assignment_submissions ON students.id = assignment_submissions.student_id 
+FROM  assignment_submissions
+JOIN  students ON students.id = student_id
 WHERE students.name = 'Ibrahim Schimmel';
+
+
+
--- 2_queries_joins/3_cohorts_total_assignment_duration.sql
+++ 2_queries_joins/3_cohorts_total_assignment_duration.sql
@@ -1,8 +1,5 @@
 SELECT SUM(assignment_submissions.duration) as total_duration
 FROM cohorts
-JOIN students 
-ON cohorts.id= students.cohort_id
-JOIN assignment_submissions 
-ON students.id = assignment_submissions.student_id
-WHERE cohorts.name= 'FEB12';
-
+JOIN students ON students.cohort_id = cohorts.id
+JOIN assignment_submissions ON students.id= assignment_submissions.student_id
+WHERE cohorts.name='FEB12'
